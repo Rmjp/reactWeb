313d644155cd6b3070c6d4a14a885c5690c81b63
Rmjp <rmjp15.rmt@gmail.com>
2021-12-18 22:32:21 +0700

    A

--- src/index.js
+++ src/index.js
@@ -1,66 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
-}
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
-
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
+const el = <h1>Hello World</h1>;
 // ========================================
+var i = 0;
+function ren(){
+  i++;
+  const elm = <h1>count = {i}</h1>;
+  ReactDOM.render(elm, document.getElementById('root'));
+}
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+setInterval(ren, 1000);
